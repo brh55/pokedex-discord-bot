c21a9f18b533dbdfb8ada866f7822a8a59d63874
Glitch (discord-magic-8-ball) <none>
2019-04-27 02:13:56 +0000

    👮🐫 Checkpoint ./server.js:491323/3980

--- server.js
+++ server.js
@@ -2,36 +2,55 @@
 // where your node app starts
 
 // init project
-const express = require('express');
-const fs = require('fs');
+const express = require("express");
+const fs = require("fs");
 
 const app = express();
 
-// we've started you off with Express, 
+// we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 if (!process.env.DISCORD_TOKEN) {
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/install.html');
-});
+  app.get("/", function(request, response) {
+    response.sendFile(__dirname + "/views/install.html");
+  });
+
+  app.get("/env/:token", function(request, response) {
+    fs.readFile(".env", function(err, data) {
+      if (err) {
+        return console.error(err);
+      }
+
+      var result = data
+        .toString()
+        .replace(/DISCORD_TOKEN=/g, "DISCORD_TOKEN=meow");
+
+      fs.writeFile(".env", result, "utf8", function(err) {
+        if (err) return console.log(err);
+      });
+    });
+    response.send(200);
+  });
 }
 
-
-app.get('/env', function(request, response) {
-  fs.readFile('.env', function (err, data) {
-   if (err) {
+app.get("/env", function(request, response) {
+  fs.readFile(".env", function(err, data) {
+    if (err) {
       return console.error(err);
-   }
-      var result = data.replace(/DISCORD_TOKEN=/g, 'DISCORD_TOKEN=meow');
+    }
+    console.log("Asynchronous read: " + data.toString());
 
-  fs.writeFile('.env', result, 'utf8', function (err) {
-     if (err) return console.log(err);
+    var result = data
+      .toString()
+      .replace(/DISCORD_TOKEN=/g, "DISCORD_TOKEN=meow");
+
+    fs.writeFile(".env", result, "utf8", function(err) {
+      if (err) return console.log(err);
+    });
   });
-   console.log("Asynchronous read: " + data.toString());
-});
   response.send(200);
 });
 // http://expressjs.com/en/starter/basic-routing.html
@@ -39,9 +58,7 @@ app.get('/env', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });*/
 
-
-
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
