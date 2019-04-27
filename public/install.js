fd19d978667e3105d8bd557d259cea409454c327
Glitch (discord-magic-8-ball) <none>
2019-04-27 20:55:14 +0000

    ⭐️😇 Checkpoint ./public/install.js:491323/2110 ./server.js:491323/72 ./views/install.html:491323/512

--- public/install.js
+++ public/install.js
@@ -1,11 +1,27 @@
 const checkInstallButton = document.getElementById("checkInstallButton");
-checkInstallButton.addEventListener('click', checkInstall);
+checkInstallButton.addEventListener("click", checkInstall);
+
+const refreshPageButton = document.getElementById("refreshPageButton");
+refreshPageButton.addEventListener("click", refreshPage);
+
+const connectedSection = document.getElementById("checkInstallButton");
 
 
-function checkInstall(){
+function checkInstall() {
   console.log("test");
- return fetch("/checkinstall/")
-    .then(res => console.log(res))
+  return fetch("/checkinstall/").then(res => {
+    console.log(res);
+    if (res.status === 200) {
+      //awesome the token is working!
+        
+      } else {
+      }
+    return Promise.resolve();
+  });
 }
 
 
+function refreshPage(){
+ location.reload(); 
+
+}
