56881ce7ad7f9c67168e9acdaee9234b4c41ad70
Glitch (discord-magic-8-ball) <none>
2019-04-28 02:44:44 +0000

    🚨🌎 Checkpoint ./public/client.js:491323/1510 ./views/index.html:491323/776

--- public/client.js
+++ public/client.js
@@ -1,18 +1,39 @@
 let domainName;
+let clientID;
+let addBotLink;
 const domainNameTitle = document.getElementById("domain-name-title");
+const addBotCopy = document.getElementById("add-bot-link");
+
+
+const oauthUrl = document.getElementById("oauth_url");
+oauthUrl.style.display = "none";
+
+const no-client-found = document.getElementById("no-client-found");
 
 function getDomain() {
   console.log("test");
-  return fetch("/domainname/")
+  return fetch("/clientid/")
     .then(res => res.json())
     .then(resJson => {
       if (resJson.error) {
         console.log(resJson.error);
       } else {
-        console.log(resJson.message);
-        domainName = resJson.message;
+        domainName = resJson.domain;
+        clientID = resJson.client;
+
         console.log("domain name " + domainName);
         domainNameTitle.innerHTML = domainName;
+        if (clientID) {
+          addBotLink= "https://discordapp.com/api/oauth2/authorize?client_id="+clientID+"&permissions=0&scope=bot"
+          oauthUrl.value = addBotLink;
+          oauthUrl.style.display = "block";
+
+        } else {
+          
+          
+        
+        }
+
       }
       return Promise.resolve();
     });
