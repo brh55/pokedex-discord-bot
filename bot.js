0df97acb8a5d67ea5d469624f77d2e97ba31640c
Glitch (discord-magic-8-ball) <none>
2019-04-29 22:18:59 +0000

    🍧🚠 Checkpoint ./bot.js:491323/675

---
+++ bot.js
@@ -0,0 +1,29 @@
+function startBot () {
+    const discordBotkit = require('botkit-discord');
+
+const configuration = {
+	token: process.env.DISCORD_TOKEN
+};
+
+const discordBot = discordBotkit(configuration);
+
+discordBot.hears('.*', 'mention', (bot, message) => {
+	const responses = [
+		"It is certain",
+		"It is decidedly so",
+		"Without a doubt",
+		"Yes – definitely",
+		"You may rely on it",
+		"As I see it",
+		"yes",
+		"Most Likely",
+		"Outlook good",
+		"Yes",
+		"Signs point to yes"
+	];
+
+	const randomIndex = Math.floor(Math.random() * responses.length);
+	bot.reply(message, responses[randomIndex]);
+});
+}
+module.exports = startBot;
