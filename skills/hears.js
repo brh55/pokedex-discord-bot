d96adf178aa9eecf491bd369671d11c2d492effc
Glitch (discord-magic-8-ball) <none>
2019-05-17 22:50:53 +0000

    😹🐽 Checkpoint ./skills/hears.js:491323/246

--- skills/hears.js
+++ skills/hears.js
@@ -1,9 +1,10 @@
 module.exports = function(controller) {
-  controller.hears(['^thanks (.*)','^thanks'], "mention", (bot, message) => {
+  controller.hears("!thanks", ["direct_mention", "mention"], (bot, message) => {
     let userID = message.userID;
-    let response = "Kudos from <@" + userID + ">";
+    let response = "Kudos from <@" + userID + "> 🎉";
     console.log(message);
 
     bot.reply(message, response);
+    console.log(response)
   });
 };
