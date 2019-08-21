85f9a1a7e2a279c70b159c452b7dfd8c54e90843
Glitch (pricey-nut) <none>
2019-08-21 15:03:42 +0000

    🐯🏵 Updated with Glitch

--- guides.js
+++ guides.js
@@ -3,7 +3,6 @@
 const express = require("express");
 const router = express.Router();
 const discordBotkit = require("botkit-discord");
-const Client = require("uptime-robot");
 const discordBot = require("./bot");
 
 router.get("/guide", function(request, response) {
@@ -25,9 +24,6 @@ router.get("/botinfo", async function(request, response) {
   let authURL;
   let domain = process.env.PROJECT_DOMAIN;
   let uptime = process.uptime();
-  let uptimeRobot;
-  let uptimeRobotApiKey = false;
-  let uptimeRobotMonitor = false;
   
   try {
      authURL = "https://discordapp.com/api/oauth2/authorize?client_id=" +
@@ -37,84 +33,14 @@ router.get("/botinfo", async function(request, response) {
     console.log("Error caught");
   }
 
-
-  try {
-    uptimeRobot = await new Client(process.env.UPTIME_ROBOT_KEY);
-    uptimeRobotApiKey = true;
-  } catch (e) {
-    console.log("Error caught");
-  }
-
-  try {
-    let monitors = await uptimeRobot.getMonitors();
-
-    monitors.forEach(function(monitor) {
-      if (
-        monitor.url ==
-        "https://" + process.env.PROJECT_DOMAIN + ".glitch.me"
-      ) {
-        uptimeRobotMonitor = true;
-      }
-    });
-  } catch (e) {
-    console.log("Error caught");
-  }
-
   response.status(200).json({
     url: authURL,
     domain: domain,
-    uptime: uptime,
-    uptimeRobotApiKey: uptimeRobotApiKey,
-    uptimeRobotMonitor: uptimeRobotMonitor
+    uptime: uptime
   });
 });
 
-router.get("/monitor", function(request, response) {
-  let uptime = process.uptime();
-  try {
-    const uptimeRobot = new Client(process.env.UPTIME_ROBOT_KEY);
-    uptimeRobot.getMonitors({}, function(err, monitors) {
-      if (err) throw response.status(200).json(err);
-      let setup = 1;
-      monitors.forEach(function(monitor) {
-        if (
-          monitor.url ==
-          "https://" + process.env.PROJECT_DOMAIN + ".glitch.me"
-        ) {
-          setup = 2;
-        }
-      });
 
-      response.status(200).json({
-        status: setup,
-        uptime: uptime
-      });
-    });
-  } catch (error) {
-    console.log("error");
-    response.status(200).json({
-      status: 0,
-      uptime: uptime
-    });
-  }
-});
-
-router.get("/createMonitor", function(request, response) {
-  const uptimeRobot = new Client(process.env.UPTIME_ROBOT_KEY);
-
-  uptimeRobot.newMonitor(
-    {
-      friendlyName: process.env.PROJECT_DOMAIN,
-      url: "https://" + process.env.PROJECT_DOMAIN + ".glitch.me",
-      type: 1,
-      interval: 5
-    },
-    function(err, res) {
-      if (err) throw response.status(200).json(err);
-      response.status(200).json(res);
-    }
-  );
-});
 
 router.get("/checkinstall", function(request, response) {
   if (!process.env.DISCORD_TOKEN) {
